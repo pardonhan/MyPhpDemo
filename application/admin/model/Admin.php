de2eba8fb9016c78e1a448c0678e0658bba529eb
hanfenglin <han_fenglin@163.com>
2018-02-07 14:12:51 +0800

    git init

---
+++ application/admin/model/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+use think\Model;
+
+/**
+ * Created by PhpStorm.
+ * User: HanFL
+ * Date: 2018/2/7
+ * Time: 14:08
+ */
+class Admin extends Model
+{
+
+    public function login($data)
+    {
+        $user = db('admin')->where('username', $data['username'])->find();
+        if ($user) {
+            if ($user['pwd'] == md5($data['password'])) {
+                session('username', $user['username']);
+                session('admin_id', $user['admin_id']);
+                return 1;//正确
+            } else {
+                return -1;//密码错误
+            }
+        } else {
+            return -2;//用户不存在
+        }
+    }
+}
